7d635c23e39459cc75d673e5ecbe4dabd3f3ed85
Ori <leesulgi66@naver.com>
2023-09-04 16:20:53 +0900

    init commit

---
+++ src/main/java/com/example/blizzardAPI/BlizzardApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.blizzardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlizzardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlizzardApiApplication.class, args);
+	}
+
+}
